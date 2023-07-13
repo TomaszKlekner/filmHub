b2568399bff9297204de6bca4c476b900c4aa799
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-07-13 23:11:35 +0200

    Fetch movie details and display them

--- js/script.js
+++ js/script.js
@@ -52,6 +52,65 @@ async function getPopularMovies() {
   });
 }
 
+// Display Mobie Details
+async function getMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIData(`movie/${movieId}?language=en-US`);
+  console.log(movie);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <div class="details-top">
+      <div>
+      ${
+        movie.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class='card-img-top' alt='${movie.title}' />
+        `
+          : `
+          <img src='images/no-image.jpg' class='card-img-top' alt='${movie.title}' />
+        `
+      }
+      </div>
+      <div>
+        <h2>${movie.title}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${movie.vote_average}
+        </p>
+        <p class="text-muted">Release Date: ${movie.release_date}</p>
+        <p>
+        ${movie.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${
+          movie.homepage
+        }" target="_blank" class="btn">Visit Movie Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span> ${numberWithCommas(
+          movie.budget
+        )}</li>
+        <li><span class="text-secondary">Revenue:</span> ${numberWithCommas(
+          movie.revenue
+        )}</li>
+        <li><span class="text-secondary">Runtime:</span> ${movie.runtime}</li>
+        <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">${movie.production_companies
+        .map((company) => `<li>${company.name}</li>`)
+        .join('')}</div>
+    </div>
+  `;
+  document.getElementById('movie-details').appendChild(div);
+}
+
 // Fetch Data from API
 async function fetchAPIData(endpoint) {
   showSpinner();
@@ -80,6 +139,11 @@ function highlightActiveLink() {
   });
 }
 
+// Add comas to numbers
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 // Init App
 function init() {
   switch (global.currentPage) {
@@ -92,6 +156,7 @@ function init() {
       break;
     case '/movie-details.html':
       console.log('Movie Details');
+      getMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details');
