dcc23a4f1fca5be1eade382e213475015d91f3ac
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-07-17 19:16:21 +0200

    Display movie detail background image

--- js/script.js
+++ js/script.js
@@ -58,6 +58,9 @@ async function getMovieDetails() {
   const movie = await fetchAPIData(`movie/${movieId}?language=en-US`);
   console.log(movie);
 
+  // Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
   div.innerHTML = `
     <div class="details-top">
@@ -115,6 +118,28 @@ async function getMovieDetails() {
   document.getElementById('movie-details').appendChild(div);
 }
 
+// Display backdrop on details page
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.getElementById('movie-details').appendChild(overlayDiv);
+  } else {
+    document.getElementById('show-details').appendChild(overlayDiv);
+  }
+}
+
 // Fetch Data from API
 async function fetchAPIData(endpoint) {
   showSpinner();
@@ -128,7 +153,6 @@ async function fetchAPIData(endpoint) {
 function showSpinner() {
   document.querySelector('.spinner').classList.add('show');
 }
-
 function hideSpinner() {
   document.querySelector('.spinner').classList.remove('show');
 }
